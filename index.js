f998a6e8528c0b6526bc8e58b796d01e3877b525
jonschlinkert <jon.schlinkert@sellside.com>
2018-04-15 12:08:10 -0400

    make variable names stricter

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function() {
-  return /(?:(['"])[^\1]*require[^\1]*\1)|\s*(?:(?:var|const|let)?\s*([\s\S]+?)\s*=\s*)?require\((['"])((?:@([^/]+?)\/)?([^/]+?)|[\s\S]+?)\3\);?/g;
+  return /(?:(['"])\s*[_.\w/$]*?\s*require\s*[^\1]*?\s*\1)|\s*(?:(?:var|const|let)?\s*([_.\w/$]+?)\s*=\s*)?require\((['"])((?:@([^/]+?)\/)?([^/]+?)|[-.@\w/$]+?)\3\);?/g;
 };
